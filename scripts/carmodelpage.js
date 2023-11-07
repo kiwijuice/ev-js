129026547fbef44a7b6a661613c5d590feeda619
kiwijuice <kiwijuice@users.noreply.github.com>
2023-11-07 09:37:45 +0200

    Update carmodelpage.js

--- scripts/carmodelpage.js
+++ scripts/carmodelpage.js
@@ -1,10 +1,17 @@
 
-// "container-wrapper" "mag-box-title the-global-title" h3/h2
-
 addEventListener('load', (event) => {
-    let x = jQuery('.mag-box-title h3');
-    console.log(x.length);
-    console.log(x);
+    let headings = jQuery('.mag-box-title h3');
+    let titles = [];
+
+    headings.each(function(i) {
+        let heading = jQuery(this);
+        let headingId = 'car-mode-heading-' + i;
+        heading.attr('id', headingId);
+        titles.push([ heading.text().replace(/[\n\t]+/g, ''),  headingId]);
+    });
+
+    let toc = jQuery('#model-page-toc ul');
+    titles.forEach((title) => {
+        toc.append(jQuery('<li></li>').text(title[0]));
+    });
 });
-
-
